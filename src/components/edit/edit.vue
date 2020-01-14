a3fe5872061a3718c4c72fba0895bff3b655b513
gwhr <guowei102617@163.com>
2020-01-14 17:58:46 +0800

    fix: '编辑页样式修改' ：#

--- src/components/edit/edit.vue
+++ src/components/edit/edit.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="edit_div">
         <article class="edit-card">
-            <div class="edit_title">{{title}}</div>
+            <input class="edit-title" type="text" v-model="title">
             <textarea class="edit_content" name="" id="" cols="30" rows="10" v-model = 'input'></textarea>
         </article>
         <article class="edit-card" id="content_card">
@@ -10,24 +10,37 @@
         </article>
         <!-- <textarea name="" id="" cols="100" rows="10" v-model="input" style="width:100%"></textarea> -->
         
-        <span class="edit_tags" @click="save" title="标签">
+        <div class="edit_tags" @click="save('tags')" title="标签" style="z-index:3">
                 <i class="iconfont icon-tag"></i>
-        </span>
-        <span class="edit_classify" @click="save" title="分类">
-                <i class="iconfont icon-icon"></i>
-        </span>
-        <span class="edit_save" @click="save" title="保存">
-                <i class="iconfont icon-baocun"></i>
-        </span>
-        <transition name="dialog" >
-            <div class="dialog" v-if="show">
-                <div class="dialog_content">
-                    <div class="dialog_button">
-                        <span @click="close">关闭</span>
+                <div class="popover" v-if="tagsshow" @click.stop>
+                    <div class="tags_list">
+                        <span v-for="(item) in tags" :key="item.id" >
+                            {{item.name}}
+                        </span>
                     </div>
+                    
                 </div>
-            </div>
-        </transition>
+                <span class="select_tags">
+                    {{selectTags.name}}
+                </span>
+        </div>
+        <div class="edit_classify" @click="save('classify')" title="分类" style="z-index:2">
+                <i class="iconfont icon-icon"></i>
+                <div class="popover" v-if="classifyshow" @click.stop>
+                    <div class="tags_list">
+                        <span v-for="(item) in classify" :key="item.id" >
+                            {{item.name}}
+                        </span>
+                    </div>
+                    
+                </div>
+                <span class="select_tags">
+                    {{selectClassify.name}}
+                </span>
+        </div>
+        <div class="edit_save" @click="save" title="保存">
+                <i class="iconfont icon-baocun"></i>
+        </div>
         
     </div>
 </template>
@@ -44,22 +57,62 @@ export default {
         const pss = new PerfectScrollbar(container,{
             suppressScrollX:true
         });
+        this.selectTags = this.tags[0];
+        this.selectClassify = this.classify[0];
     },
     created() {
     },
     data(){
         return {
-            show:false,
+            // 显示的下标
+            tagsshow:false,
+            classifyshow:false,
+            tags:[
+                {
+                    name:'Js',
+                    id:1
+                },
+                {
+                    name:'HTML',
+                    id:2
+                },
+                {
+                    name:'Vue',
+                    id:3
+                },
+                {
+                    name:'Node',
+                    id:4
+                },
+
+            ],
+            classify:[
+                 {
+                    name:'博客',
+                    id:1
+                },
+                {
+                    name:'HTML',
+                    id:2
+                },
+            ],
+            selectTags:{},
+            selectClassify:{},
             title:'萨达萨达',
             input:'> 我又滚回来了，以后大概也许每周会写一篇博客，就算是一篇水文也行，就像下面要写的这篇，**多边形的音乐动效**，姑且就叫这个名字，就是基于`audio`的`AnalyserNode.getByteTimeDomainData() `API，再结合以前做过的**多变形Demo**做出来的一个小玩意，说实话，最后做出来，感觉效果挺一般的，不够惊艳。'
         }
     },
     methods:{
-        save(){
-            this.show=!this.show;
+        save(name){
+            
+            this[`${name}show`]=!this[`${name}show`];
         },
         close(){
             this.show= false;
+        },
+        // 获取简介
+        getDescribe(){
+            
         }
     }
 }
